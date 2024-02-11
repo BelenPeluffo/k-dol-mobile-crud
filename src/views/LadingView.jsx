8b0f833f685ebaca01c67fad814dd9a045700fdc
Belén Peluffo <belenpeluffolupiano@gmail.com>
2024-02-11 15:34:16 -0300

    feat: creates main navigation screen.

--- src/views/LadingView.jsx
+++ src/views/LadingView.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, Text, View } from "react-native";
+import { Navigation } from "react-native-navigation";
 
 const LandingView = () => {
   return (
@@ -7,10 +8,14 @@ const LandingView = () => {
       <Text style={{ fontSize: 50, width: "100%", marginBottom: 10 }}>
         Welcome to kIdol CRUD
       </Text>
-      <Text style={{ fontSize: 20, marginBottom: 20 }}>What R we going to do today?</Text>
-      <Button title="Go to CRUD" onPress={}></Button>
+      <Text style={{ fontSize: 20, marginBottom: 20 }}>
+        What R we going to do today?
+      </Text>
+      <Button title="Go to CRUD" /*onPress={}*/></Button>
     </View>
   );
 };
+
+Navigation.registerComponent('Home', () => LandingView);
 
 export default LandingView;
