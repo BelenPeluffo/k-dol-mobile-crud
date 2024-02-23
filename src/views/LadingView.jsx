24476c4d65faffb7c4ec3401720c668d047a20f1
Belén Peluffo <belen.peluffo@dirmod.com>
2024-02-23 14:18:37 -0300

    feat (LandingView.jsx): corrects view styles.

--- src/views/LadingView.jsx
+++ src/views/LadingView.jsx
@@ -4,8 +4,21 @@ import { Navigation } from "react-native-navigation";
 
 const LandingView = ({ navigation }) => {
   return (
-    <View style={{ alignItems: "center" }}>
-      <Text style={{ fontSize: 50, width: "100%", marginBottom: 10 }}>
+    <View
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 1,
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 50,
+          width: "100%",
+          marginBottom: 10,
+          textAlign: "center",
+        }}
+      >
         Welcome to kIdol CRUD
       </Text>
       <Text style={{ fontSize: 20, marginBottom: 20 }}>
